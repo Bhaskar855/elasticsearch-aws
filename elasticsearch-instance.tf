b653d9174a6c0b68f69d8276fbccd075f640a70e
Bhaskar855 <56507117+Bhaskar855@users.noreply.github.com>
2020-05-22 19:48:12 +0530

    Update elasticsearch-instance.tf

--- elasticsearch-instance.tf
+++ elasticsearch-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "elasticsearch_instance" {
-  ami           = "ami-07c1207a9d40bc3bd"
+  ami           = "ami-0a54aef4ef3b5f881"
   instance_type = "t2.micro"
-  security_groups = ["sg-009ff540c487175bc"]
+  security_groups = ["launch-wizard-16"]
   iam_instance_profile = "${aws_iam_instance_profile.elasticsearch_profile.name}"
   user_data = "${file("elasticsearch-node-setup.sh")}"
   count = "2"
