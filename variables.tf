6f5fe2ed9fd386b7b549a9278ad749ce4cbc266b
Bhaskar855 <56507117+Bhaskar855@users.noreply.github.com>
2020-05-23 18:36:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "aws_access_key" {}
+variable "aws_access_key" "AKIAYR5PC2SI5VR6YY53"
 
-variable "aws_secret_key" {}
+variable "aws_secret_key" "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"
 
 # Need to provide a Red-Hat based image
 variable "aws_ami" {
